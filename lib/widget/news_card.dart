078b3a96beef3172c73da802f2da133a6adbb72a
Nishant Niroula <niroulanishant910@gmail.com>
2025-01-12 23:19:54 +0545

    share icon and url luncher

--- lib/widget/news_card.dart
+++ lib/widget/news_card.dart
@@ -1,22 +1,56 @@
 import 'package:flutter/material.dart';
 import '../constants/url_constants.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+_launchUrl(String url) async {
+  if (!await launchUrl(Uri.parse(url))) {
+    throw Exception('Could not launch $url');
+  }
+}
 
 getNewsCard(e) {
-  return Column(
-    children: [
-      Image.network(e["urlToImage"] ?? PLACEHOLDER_IMAGE_URL),
-      Padding(
-        padding: const EdgeInsets.all(8.0),
+  return Stack(children: [
+    Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 98, 175, 189),
+            borderRadius: BorderRadius.circular(12.0)),
         child: Column(
           children: [
-            Text(
-              e["title"],
-              style: const TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Text(e["description"] ?? "...")
+            Image.network(e["urlToImage"] ?? PLACEHOLDER_IMAGE_URL),
+            GestureDetector(
+              onTap: () => _launchUrl(e["url"]),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Text(
+                      e["title"],
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 255, 255, 255),
+                          fontSize: 17,
+                          fontStyle: FontStyle.normal),
+                    ),
+                    const SizedBox(
+                      height: 8.0,
+                    ),
+                    Text(e["description"] ?? "...")
+                  ],
+                ),
+              ),
+            )
           ],
         ),
-      )
-    ],
-  );
+      ),
+    ),
+    const Positioned(
+        top: 10,
+        right: 10,
+        child: Icon(
+          Icons.share,
+          color: Colors.white,
+        ))
+  ]);
 }
