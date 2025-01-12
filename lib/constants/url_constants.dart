6babd88ad19afd7f20ae1cc2eca68325aeae4264
Nishant Niroula <niroulanishant910@gmail.com>
2025-01-12 19:03:23 +0545

    updated igonre files

---
+++ lib/constants/url_constants.dart
@@ -0,0 +1,5 @@
+// ignore: constant_identifier_names
+const String API_KEY = "3e44a08d3b4f445b9fc6a96069f40883";
+// ignore: constant_identifier_names
+const String NewsApiKey =
+    "https://newsapi.org/v2/top-headlines?country=us&apiKey=$API_KEY";
